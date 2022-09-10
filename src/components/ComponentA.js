aeb80697236608a8c825a611df94b19212f2b2a4
SANDRO SIMON <sandrosimonore@gmail.com>
2022-09-10 12:53:21 -0500

    initial commit

---
+++ src/components/ComponentA.js
@@ -0,0 +1,11 @@
+import { Contact } from "../models/contact.class";
+import PropTypes from 'prop-types';
+const ComponentA = (props) => {
+    
+}
+
+ComponentA.PropTypes = {
+
+}
+
+export default ComponentA;
---
+++ src/models/contact.class.js
@@ -0,0 +1,13 @@
+export class Contact {
+    firstName = '';
+    lastName = '';
+    email= '';
+    online = '';
+
+    constructor(firstName, lastName, email, online){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.online = online;
+    }
+}
